cc31fda6bb46a6e7859ebc6a2f9fc976dcf76d01
Conor O'Mahony <conchuromathuna@gmail.com>
2020-05-23 10:49:23 -0400

    Restrict access to data for each user

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True) 
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
@@ -20,7 +22,4 @@ class Entry(models.Model):
  
     def __str__(self):
         """Return a string representation of the model."""
-        if len(self.text) > 50:
-            return f"{self.text[:50]}..."
-        else:
-            return f"{self.text}"
+        return f"{self.text[:50]}..."
